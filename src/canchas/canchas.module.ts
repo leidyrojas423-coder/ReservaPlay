f6c84909d0700f9173d39a84b86ea5c4269dba11
Angela Eulena martinez Gomez <angelcris1027@gmail.com>
2026-07-18 13:32:03 -0500

    chore: mantener versión oficial de canchas.module.ts

--- src/canchas/canchas.module.ts
+++ src/canchas/canchas.module.ts
@@ -1,21 +1,14 @@
 import { Module } from '@nestjs/common';
 import { TypeOrmModule } from '@nestjs/typeorm';
 import { AdministradorEntity } from '../administradores/entities/administrador.entity';
+import { HorarioEntity } from '../horarios/entities/horario.entity';
+import { ReservaEntity } from '../reservas/entities/reserva.entity';
 import { CanchasController } from './canchas.controller';
 import { CanchasService } from './canchas.service';
 import { CanchaEntity } from './entities/cancha.entity';
-import { HorarioEntity } from '../horarios/entities/horario.entity';
-import { ReservaEntity } from '../reservas/entities/reserva.entity';
 
 @Module({
-  imports: [
-    TypeOrmModule.forFeature([
-      CanchaEntity,
-      AdministradorEntity,
-      HorarioEntity,
-      ReservaEntity,
-    ]),
-  ],
+  imports: [TypeOrmModule.forFeature([CanchaEntity, AdministradorEntity, HorarioEntity, ReservaEntity])],
   controllers: [CanchasController],
   providers: [CanchasService],
 })
