28916cb7cc6cc2293a00182046a43400b8e83c0f
Angela Eulena martinez Gomez <angelcris1027@gmail.com>
2026-07-18 12:07:53 -0500

    Cambios en configuración y módulo de canchas

--- src/canchas/canchas.module.ts
+++ src/canchas/canchas.module.ts
@@ -4,9 +4,18 @@ import { AdministradorEntity } from '../administradores/entities/administrador.e
 import { CanchasController } from './canchas.controller';
 import { CanchasService } from './canchas.service';
 import { CanchaEntity } from './entities/cancha.entity';
+import { HorarioEntity } from '../horarios/entities/horario.entity';
+import { ReservaEntity } from '../reservas/entities/reserva.entity';
 
 @Module({
-  imports: [TypeOrmModule.forFeature([CanchaEntity, AdministradorEntity])],
+  imports: [
+    TypeOrmModule.forFeature([
+      CanchaEntity,
+      AdministradorEntity,
+      HorarioEntity,
+      ReservaEntity,
+    ]),
+  ],
   controllers: [CanchasController],
   providers: [CanchasService],
 })
